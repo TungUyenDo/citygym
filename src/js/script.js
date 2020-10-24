e7ee6a768de78729c42eb5608dd7d370242b8687
UyenDo <uyendtt@minerva.vn>
2020-10-24 12:42:55 +0700

    update 24

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,18 @@
 
+
+$('.ct-menu__toggle').click(function (e) {
+    e.preventDefault();
+    $('.ct-menu__nav').toggleClass('active')
+    $('.ct-menu__nav-item').show()
+    $('body').toggleClass('sidebar-menu-open')
+})
+$('.ct-menu__toggle-mobile').click(function (e) {
+    e.preventDefault();
+    $('.ct-menu__nav').removeClass('active')
+    $('body').removeClass('sidebar-menu-open')
+})
+
+
 $(window).on("load", function () {
 });
 
